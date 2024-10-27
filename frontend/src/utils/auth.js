b26d212b78b1e3e83417522796ded6b1c451de40
Gabriel Islas <gabrielislas10@hotmail.com>
2024-10-26 23:26:52 -0600

    domain url active

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,12 +1,10 @@
-/* export const BASE_URL = 'https://api.gabriel14.mooo.com'; */
-
-export const BASE_URL = "https://api.gabriel14.mooo.com";
+export const BASE_URL = 'https://api.gabriel14.mooo.com';
 
 export const register = async (email, password) => {
   try {
     const response = await fetch(`${BASE_URL}/signup`, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ email, password }),
     });
 
@@ -17,7 +15,7 @@ export const register = async (email, password) => {
     const data = await response.json();
     return data;
   } catch (err) {
-    console.error("Error:", err);
+    console.error('Error:', err);
     throw err;
   }
 };
@@ -25,11 +23,11 @@ export const register = async (email, password) => {
 export const login = async (email, password) => {
   try {
     const response = await fetch(`${BASE_URL}/signin`, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ email, password }),
     });
-    console.log("🚀 ~ login ~ response:", response);
+    console.log('🚀 ~ login ~ response:', response);
 
     if (!response.ok) {
       throw new Error(`Error: ${response.status}`);
@@ -38,18 +36,18 @@ export const login = async (email, password) => {
     const data = await response.json();
     return data;
   } catch (err) {
-    console.error("Error:", err);
-    throw new Error("Correo o contraseña incorrectos");
+    console.error('Error:', err);
+    throw new Error('Correo o contraseña incorrectos');
   }
 };
 
 export const getUserToken = async (token) => {
   try {
     const response = await fetch(`${BASE_URL}/users/me`, {
-      method: "GET",
+      method: 'GET',
       headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
         Authorization: `Bearer ${token}`,
       },
     });
@@ -61,7 +59,7 @@ export const getUserToken = async (token) => {
     const data = await response.json();
     return data;
   } catch (err) {
-    console.error("Error:", err);
+    console.error('Error:', err);
     throw err;
   }
 };
